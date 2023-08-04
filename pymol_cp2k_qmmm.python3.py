de1ceee7d80c6a42621032259bf2bb0a4c6b4104
Miquel Canyelles Niño <mcanyellesnino@gmail.com>
2023-08-04 10:48:28 +0200

    Added original header (from original script) with license and author(s)

--- pymol_cp2k_qmmm.python3.py
+++ pymol_cp2k_qmmm.python3.py
@@ -1,3 +1,30 @@
+# PyMol script to generate/load part of the &QMMM section of CP2K input
+# Copyright (C) 2015 Peter Mamonov <pmamonov@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# USAGE:
+#   gen_cp2k_qmmm filename, qm_part_selection_name, the_whole_system_object
+#
+#   N.B.: The output is written to file "filename". "Filename" is overwritten!
+#
+# REFERENCES:
+#   CP2K: http://cp2k.berlios.de/
+#   PyMol: http://www.pymol.org/
+#
+# Originally written by Peter Mamonov. Adapted to Python3 and PyMOL2.5 by Miquel Canyelles.
+
 from pymol import cmd, stored
 import re
 
